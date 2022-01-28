d0883afe3f9f6f65eae0df03adf100f8b5269205
Abraham Kays <abrahamk@ksu.edu>
2022-01-28 10:29:47 -0600

    initial commit

---
+++ ElaphantsWithMachineGuns/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ElaphantsWithMachineGuns
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
